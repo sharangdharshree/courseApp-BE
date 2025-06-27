966867308a9fc80d8500a26c6cecc19fa7a28eed
Sharangdhar Shree <sharangdhar7984@gmail.com>
2025-06-28 02:10:52 +0530

    given course route and controller outline

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,1 +1,25 @@
 // will deal with access of course with its full contents, only authorized user can be able to access
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import Course from "../models/course.model.js";
+
+const getCourse = asyncHandler(async (req, res) => {});
+
+const addSection = asyncHandler(async (req, res) => {});
+const updateSection = asyncHandler(async (req, res) => {});
+const deleteSection = asyncHandler(async (req, res) => {});
+const addContent = asyncHandler(async (req, res) => {});
+const updateContent = asyncHandler(async (req, res) => {});
+const deleteContent = asyncHandler(async (req, res) => {});
+
+export {
+  getCourse,
+  addSection,
+  updateSection,
+  deleteSection,
+  addContent,
+  updateContent,
+  deleteContent,
+};
--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -1,10 +1,40 @@
 import { Router } from "express";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+import {
+  getCourse,
+  addSection,
+  updateSection,
+  deleteSection,
+  addContent,
+  updateContent,
+  deleteContent,
+} from "../controllers/course.controller";
+import { validate } from "../middlewares/validate.middleware.js";
+import { sectionSchema, contentSchema } from "../schemas/course.schema.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-export default router;
+// routes for user
+// get course contents
+router.route("/:id/learn").get(verifyJwt, getCourse);
 
-// here course has already been created by the admin
-// now we will deal with with adding any content or section in the course, this operation will be done by valid authorized admin
-// will fetch complete course with its content and sections, this operation will be done by valid authorized user for consumption
-// route should look like /user/course/courseId or /admin/course/courseId/operations, check how to achieve this
+// routes for admin
+// add section, update section, delete section, add content, update content, delete content
+router
+  .route("/:id/add-section")
+  .post(verifyJwt, validate(sectionSchema), addSection);
+router
+  .route("/:id/update-section")
+  .put(verifyJwt, validate(sectionSchema), updateSection);
+router.route("/:id/delete-section").delete(verifyJwt, deleteSection);
+
+router
+  .route("/:id/add-content")
+  .post(verifyJwt, validate(contentSchema), addContent);
+router
+  .route("/:id/update-content")
+  .put(verifyJwt, validate(contentSchema), updateContent);
+router.route("/:id/delete-content").delete(verifyJwt, deleteContent);
+
+export default router;
